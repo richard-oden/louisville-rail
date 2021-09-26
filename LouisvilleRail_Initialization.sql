009f1195105216f1253b620c84495891fe407094
Richard Oden <richard.thomas.oden@gmail.com>
2021-09-26 17:32:40 -0400

    Created LineStop table. Revised and renamed Line table.

--- LouisvilleRail_Initialization.sql
+++ LouisvilleRail_Initialization.sql
@@ -18,9 +18,18 @@ GO
 USE LouisvilleRailNetwork;
 GO
 
-CREATE TABLE RailLine (
-	LineId int identity(1,1) NOT NULL,
-	RouteName nvarchar(255),
+CREATE TABLE Line (
+	Id int identity(1,1) NOT NULL,
+	LineName nvarchar(255),
+	CONSTRAINT PK_Line PRIMARY KEY LineId,
+	CONSTRAINT FK_LineStop FOREIGN KEY (LineId) REFERENCES Line (Id)
+);
+GO
+
+CREATE TABLE LineStop (
+	LineId int NOT NULL,
+	StopId int NOT NULL,
+	LineStopOrder int NOT NULL,
 	CONSTRAINT PK_RailLine PRIMARY KEY RouteId,
 	CONSTRAINT FK_RailLineStops FOREIGN KEY (RouteId) REFERENCES RailLine (LineId)
 );
