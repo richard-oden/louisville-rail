2654d29589b63480ea85c250772f2aeda0c677cf
Richard Oden <richard.thomas.oden@gmail.com>
2021-09-26 18:30:09 -0400

    Added Stop table

--- LouisvilleRail_Initialization.sql
+++ LouisvilleRail_Initialization.sql
@@ -1,36 +1,24 @@
 USE master;
-GO
 
-IF DATABASEPROPERTYEX ('LouisvilleRailNetwork','Version') IS NOT NULL
-BEGIN
-	ALTER DATABASE LouisvilleRailNetwork SET SINGLE_USER
-		WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE LouisvilleRailNetwork;
-END
-GO
+DROP DATABASE IF EXISTS LouisvilleRailNetwork;
 
 CREATE DATABASE LouisvilleRailNetwork;
-GO
-
-ALTER DATABASE LouisvilleRailNetwork SET RECOVERY SIMPLE;
-GO
 
 USE LouisvilleRailNetwork;
-GO
 
 CREATE TABLE Line (
-	Id int identity(1,1) NOT NULL,
-	LineName nvarchar(255),
-	CONSTRAINT PK_Line PRIMARY KEY LineId,
-	CONSTRAINT FK_LineStop FOREIGN KEY (LineId) REFERENCES Line (Id)
-);
-GO
+	Id int NOT NULL PRIMARY KEY,
+	[Name] nvarchar(255));
+
+CREATE TABLE [Stop] (
+	Id int NOT NULL PRIMARY KEY,
+	[Name] varchar(255),
+	[Address] varchar(255),
+	Latitude decimal(8,6) NOT NULL,
+	Longitude decimal(9,6) NOT NULL);
 
 CREATE TABLE LineStop (
-	LineId int NOT NULL,
-	StopId int NOT NULL,
-	LineStopOrder int NOT NULL,
-	CONSTRAINT PK_RailLine PRIMARY KEY RouteId,
-	CONSTRAINT FK_RailLineStops FOREIGN KEY (RouteId) REFERENCES RailLine (LineId)
-);
-GO
+	Id int NOT NULL PRIMARY KEY,
+	LineId int NOT NULL FOREIGN KEY REFERENCES Line(Id),
+	StopId int NOT NULL FOREIGN KEY REFERENCES [Stop](Id),
+	LineStopOrder int NOT NULL);
